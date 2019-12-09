25c502b541fd3674aa3cc84695d6bbf16b3fac7e
nahknarmi <ikhan@thoughtworks.com>
2019-12-09 13:20:25 -0500

    Throwing runtime exception in case no credentials have been specified.

--- src/main/java/net/nahknarmi/arch/adapter/StructurizrAdapter.java
+++ src/main/java/net/nahknarmi/arch/adapter/StructurizrAdapter.java
@@ -7,15 +7,15 @@ import com.structurizr.api.StructurizrClientException;
 import com.structurizr.util.WorkspaceUtils;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
 
 public class StructurizrAdapter {
     private final Workspace workspace;
-    private final StructurizrClient buildClient;
 
     public StructurizrAdapter(long workspaceId) throws StructurizrClientException {
-        this.buildClient = buildClient();
+        StructurizrClient buildClient = buildClient();
         this.workspace = buildClient.getWorkspace(workspaceId);
     }
 
@@ -41,14 +41,20 @@ public class StructurizrAdapter {
 
         if (structurizrApiKey != null && structurizrApiSecret != null) {
             structurizrClient = new StructurizrClient(structurizrApiKey, structurizrApiSecret);
-        } else {
+        } else if (credentialsAsStream() != null) {
             InputStreamReader reader =
-                    new InputStreamReader(StructurizrQuickstart.class.getResourceAsStream("/structurizr/credentials.json"));
+                    new InputStreamReader(credentialsAsStream());
             Map<String, String> map = new Gson().fromJson(reader, Map.class);
             structurizrClient = new StructurizrClient(map.get("api_key"), map.get("api_secret"));
+        } else {
+            throw new IllegalStateException("Structurizr credentials could not be found. See documentation on how to configure.");
         }
         structurizrClient.setWorkspaceArchiveLocation(null);
 
         return structurizrClient;
+    }
+
+    private InputStream credentialsAsStream() {
+        return StructurizrQuickstart.class.getResourceAsStream("/structurizr/credentials.json");
     }
 }

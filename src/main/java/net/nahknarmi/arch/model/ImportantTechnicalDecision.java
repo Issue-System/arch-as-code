ec2c557a9502327cba9661880f9349c66dd5cf18
nahknarmi <ikhan@thoughtworks.com>
2019-12-10 16:36:49 -0500

    Adding Lombok support

--- src/main/java/net/nahknarmi/arch/model/ImportantTechnicalDecision.java
+++ src/main/java/net/nahknarmi/arch/model/ImportantTechnicalDecision.java
@@ -1,7 +1,12 @@
 package net.nahknarmi.arch.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
 public class ImportantTechnicalDecision {
     private String id;
     private Date date;
@@ -9,54 +14,6 @@ public class ImportantTechnicalDecision {
     private String status;
     private String content;
 
-    public ImportantTechnicalDecision() {
-    }
-
-    public ImportantTechnicalDecision(String id, Date date, String title, String status, String content) {
-        this.id = id;
-        this.date = date;
-        this.title = title;
-        this.status = status;
-        this.content = content;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return this.id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return this.content;
+    ImportantTechnicalDecision() {
     }
 }

5ef94b01a7b5204e657528e9c3f27017df0abb4f
Nikola Savic <nsavic@thoughtworks.com>
2020-01-14 15:55:31 -0500

    Remove business rule validation

--- src/main/java/net/nahknarmi/arch/domain/ArchitectureDataStructureReader.java
+++ src/main/java/net/nahknarmi/arch/domain/ArchitectureDataStructureReader.java
@@ -1,13 +1,10 @@
 package net.nahknarmi.arch.domain;
 
-import net.nahknarmi.arch.transformation.validator.DataStructureValidationException;
-import net.nahknarmi.arch.transformation.validator.ModelValidator;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.List;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -19,12 +16,7 @@ public class ArchitectureDataStructureReader {
         checkArgument(manifest.exists(), String.format("Manifest file does not exist - %s.", manifest.getAbsolutePath()));
 
         ArchitectureDataStructure dataStructure = new Yaml().loadAs(new FileInputStream(manifest), ArchitectureDataStructure.class);
-        List<String> messages = new ModelValidator().validate(dataStructure);
-        if (!messages.isEmpty()) {
-            messages.forEach(System.err::println);
-            throw new DataStructureValidationException("Validation has failed.");
-        }
-
+ 
         return dataStructure;
     }
 }

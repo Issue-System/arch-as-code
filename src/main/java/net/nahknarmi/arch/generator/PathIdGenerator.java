4447b161ead2f15fdb68e0a55a78446da0e40ebb
Nikola Savic <nsavic@thoughtworks.com>
2020-02-03 17:24:56 -0500

    Replace entity to path function with a recursive implementation.

--- src/main/java/net/nahknarmi/arch/generator/PathIdGenerator.java
+++ src/main/java/net/nahknarmi/arch/generator/PathIdGenerator.java
@@ -2,10 +2,12 @@ package net.nahknarmi.arch.generator;
 
 import com.structurizr.model.Element;
 import com.structurizr.model.IdGenerator;
+import com.structurizr.model.Person;
 import com.structurizr.model.Relationship;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
-import net.nahknarmi.arch.domain.c4.*;
+import net.nahknarmi.arch.domain.c4.C4Model;
+import net.nahknarmi.arch.domain.c4.C4Path;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -18,7 +20,7 @@ public class PathIdGenerator implements IdGenerator {
 
     @Override
     public String generateId(Element element) {
-        C4Path path = getC4Path(element);
+        C4Path path = buildPath(element);
         String pathString = path.getPath();
         return pathString;
     }
@@ -49,51 +51,20 @@ public class PathIdGenerator implements IdGenerator {
     public void found(String id) {
     }
 
-    private C4Path getC4Path(Element element) {
-        String className = element.getClass().toString();
+    private C4Path buildPath(Element element) {
+        if (element.getParent() == null) {
+            String prefix = "c4://";
 
-        C4Path path;
-        switch (className) {
-            case "class com.structurizr.model.Person":
-                C4Person c4Person = dataStructureModel.getPeople().stream()
-                        .filter(p -> p.getName().equals(element.getName()))
-                        .findFirst()
-                        .get();
-                path = c4Person.getPath();
-                break;
+            if (element instanceof Person) {
+                prefix = "@";
+            }
 
-            case "class com.structurizr.model.SoftwareSystem":
-                C4SoftwareSystem c4SoftwareSystem = dataStructureModel.getSystems().stream()
-                        .filter(s -> s.getName().equals(element.getName()))
-                        .findFirst()
-                        .get();
-                path = c4SoftwareSystem.getPath();
-                break;
-
-            case "class com.structurizr.model.Container":
-                C4Container c4Container = dataStructureModel.getContainers().stream()
-                        .filter(cont ->
-                                cont.getName().equals(element.getName()) &&
-                                        element.getParent().getName().equals(cont.getPath().getSystemName())
-                        )
-                        .findFirst()
-                        .get();
-                path = c4Container.getPath();
-                break;
-            case "class com.structurizr.model.Component":
-                C4Component c4Component = dataStructureModel.getComponents().stream()
-                        .filter(comp ->
-                                comp.getName().equals(element.getName()) &&
-                                        element.getParent().getName().equals(comp.getPath().getContainerName().get()) &&
-                                        element.getParent().getParent().getName().equals(comp.getPath().getSystemName())
-                        )
-                        .findFirst()
-                        .get();
-                path = c4Component.getPath();
-                break;
-            default:
-                throw new IllegalStateException("Unsupported element type: " + className);
+            String path = prefix + element.getName().replaceAll("/", "-");
+            return new C4Path(path);
         }
-        return path;
+
+        String c4Path = buildPath(element.getParent()).getPath();
+        String fullPath = c4Path + "/" + element.getName().replaceAll("/", "-");
+        return new C4Path(fullPath);
     }
 }

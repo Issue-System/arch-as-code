35a884dc1a5b547ffc348fb1342e950a7a044f86
nahknarmi <ikhan@thoughtworks.com>
2020-02-04 07:51:44 -0500

    Logging the path where imported architecture data structure is available

--- src/main/java/net/nahknarmi/arch/commands/ImportCommand.java
+++ src/main/java/net/nahknarmi/arch/commands/ImportCommand.java
@@ -6,6 +6,8 @@ import com.structurizr.Workspace;
 import com.structurizr.util.WorkspaceUtils;
 import net.nahknarmi.arch.adapter.WorkspaceConverter;
 import net.nahknarmi.arch.domain.ArchitectureDataStructure;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import picocli.CommandLine;
 
 import java.io.File;
@@ -13,6 +15,7 @@ import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "import", description = "Imports existing struturizr workspace")
 public class ImportCommand implements Callable<Integer> {
+    private static final Log logger = LogFactory.getLog(ImportCommand.class);
 
     @CommandLine.Parameters(index = "0", paramLabel = "EXPORTED_WORKSPACE", description = "Exported structurizr workspace location.", defaultValue = "./")
     private File exportedWorkspacePath;
@@ -30,8 +33,9 @@ public class ImportCommand implements Callable<Integer> {
         Workspace workspace = WorkspaceUtils.loadWorkspaceFromJson(exportedWorkspacePath);
         ArchitectureDataStructure dataStructure = new WorkspaceConverter().convert(workspace);
 
-        File tempFile = File.createTempFile("arch-as-code", "yml");
+        File tempFile = File.createTempFile("arch-as-code", ".yml");
         new ObjectMapper(new YAMLFactory()).writeValue(tempFile, dataStructure);
+        logger.info(String.format("Architecture data structure written to - %s", tempFile.getAbsolutePath()));
 
         return new PublishCommand(tempFile.getParentFile(), tempFile.getName()).call();
     }

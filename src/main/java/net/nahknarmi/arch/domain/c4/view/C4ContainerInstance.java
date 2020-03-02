c2dd33be99954d5e6f6c37e8ff9ae55fdca3f41c
nikolasavic-tw <nsavic@thoughtworks.com>
2020-03-02 15:26:37 -0500

    Add container instance relationships

--- src/main/java/net/nahknarmi/arch/domain/c4/view/C4ContainerInstance.java
+++ src/main/java/net/nahknarmi/arch/domain/c4/view/C4ContainerInstance.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +16,5 @@ public class C4ContainerInstance {
     private String environment;
     private String containerAlias;
     private String containerId;
+    private List<C4ContainerInstanceRelationship> relationships = emptyList();
 }

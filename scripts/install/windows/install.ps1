1ec1232bb68ba8f4514efba533e4e8a43ef19f0f
Yogesh Dhamija <45855067+yogesh-dhamija@users.noreply.github.com>
2020-03-26 16:18:20 -0400

    Attempt to fix windows script

--- scripts/install/windows/install.ps1
+++ scripts/install/windows/install.ps1
@@ -2,11 +2,11 @@ Set-PSDebug -Trace 2
 
 $download_url = (Invoke-WebRequest "https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest" | ConvertFrom-Json).assets.browser_download_url
 
-(New-Object System.Net.WebClient).DownloadFile($download_url, "$env:temp\arch-as-code.tar.gz")
-
 New-Item -ItemType Directory -Force -Path $HOME\arch-as-code
 
-tar -xzv --strip-components 1 -f $env:temp\arch-as-code.tar.gz -C $HOME\arch-as-code\
+(New-Object System.Net.WebClient).DownloadFile($download_url, "$HOME\arch-as-code\arch-as-code.tar.gz")
+
+tar -xzv --strip-components 1 -f $HOME\arch-as-code\arch-as-code.tar.gz -C $HOME\arch-as-code
 
 $Env:Path += ";$HOME\arch-as-code\bin"
 

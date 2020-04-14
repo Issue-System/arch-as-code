3b6472a5013f82f8de4af943b31eeece452f7e10
nikolasavic-tw <nsavic@thoughtworks.com>
2020-04-14 14:13:48 -0400

    Add default HttpClient details to test

--- src/test/java/net/trilogy/arch/adapter/Jira/JiraApiFactoryTest.java
+++ src/test/java/net/trilogy/arch/adapter/Jira/JiraApiFactoryTest.java
@@ -1,9 +1,11 @@
 package net.trilogy.arch.adapter.Jira;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import javax.net.ssl.SSLContext;
 import java.net.http.HttpClient;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -22,12 +24,18 @@ public class JiraApiFactoryTest {
     }
 
     @Test
-    @Ignore
-    public void shouldCreateCorrectClient() {
+    public void shouldCreateCorrectClient() throws NoSuchAlgorithmException {
         final JiraApiFactory factory = new JiraApiFactory();
 
         HttpClient client = factory.createClient();
 
-        assertThat(client.authenticator(), equalTo(null));
+        assertThat(client.connectTimeout(), equalTo(Optional.empty()));
+        assertThat(client.authenticator(), equalTo(Optional.empty()));
+        assertThat(client.cookieHandler(), equalTo(Optional.empty()));
+        assertThat(client.executor(), equalTo(Optional.empty()));
+        assertThat(client.proxy(), equalTo(Optional.empty()));
+        assertThat(client.followRedirects(), equalTo(HttpClient.Redirect.NEVER));
+        assertThat(client.sslContext(), equalTo(SSLContext.getDefault()));
+        assertThat(client.version(), equalTo(HttpClient.Version.HTTP_2));
     }
 }

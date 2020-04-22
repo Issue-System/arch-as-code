78fa4fe81a9b6aefae202374df8aebe5467a0329
Yogesh Dhamija <yogesh.dhamija@thoughtworks.com>
2020-04-22 16:06:18 -0400

    add test verifying au command prints usage

--- src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuCommandTest.java
+++ src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuCommandTest.java
@@ -1,22 +1,51 @@
 package net.trilogy.arch.e2e.architectureUpdate;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static net.trilogy.arch.TestHelper.execute;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 public class AuCommandTest {
     @Rule
     public final ErrorCollector collector = new ErrorCollector();
 
+    final PrintStream originalOut = System.out;
+    final PrintStream originalErr = System.err;
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    final ByteArrayOutputStream err = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        out.reset();
+        err.reset();
+        System.setOut(new PrintStream(out));
+        System.setErr(new PrintStream(err));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
     @Test
     public void rootCommandShouldPrintUsage() throws Exception {
-        // TODO: Assert that usage is shown
         collector.checkThat(
                 execute("au"),
                 equalTo(0)
+        );
+
+        collector.checkThat(
+                out.toString(),
+                containsString("Usage:")
         );
     }
 }

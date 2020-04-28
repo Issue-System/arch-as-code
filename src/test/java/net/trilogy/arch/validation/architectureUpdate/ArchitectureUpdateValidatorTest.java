fa0747bb862f00be9e2f6dbbba132adb513ce10e
Yogesh Dhamija <yogesh.dhamija@thoughtworks.com>
2020-04-28 14:54:30 -0400

    wip

--- src/test/java/net/trilogy/arch/validation/architectureUpdate/ArchitectureUpdateValidatorTest.java
+++ src/test/java/net/trilogy/arch/validation/architectureUpdate/ArchitectureUpdateValidatorTest.java
@@ -1,5 +1,7 @@
 package net.trilogy.arch.validation.architectureUpdate;
 
+import net.trilogy.arch.adapter.in.ArchitectureDataStructureReader;
+import net.trilogy.arch.domain.ArchitectureDataStructure;
 import net.trilogy.arch.domain.architectureUpdate.ArchitectureUpdate;
 import net.trilogy.arch.domain.architectureUpdate.CapabilitiesContainer;
 import net.trilogy.arch.domain.architectureUpdate.Decision;
@@ -13,9 +15,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import static net.trilogy.arch.TestHelper.MANIFEST_PATH_TO_TEST_MODEL_COMPONENTS;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
@@ -25,9 +30,13 @@ public class ArchitectureUpdateValidatorTest {
     public final ErrorCollector collector = new ErrorCollector();
 
     private ArchitectureUpdate invalidAu;
+    private ArchitectureDataStructure validDataStructure;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+        var file = new File(getClass().getResource(MANIFEST_PATH_TO_TEST_MODEL_COMPONENTS).getPath());
+        validDataStructure = new ArchitectureDataStructureReader().load(file);
+
         invalidAu = ArchitectureUpdate.builderPreFilledWithBlanks()
                 .decisions(Map.of(
                         new Decision.Id("Missing-TDD-Decision-1"), new Decision("Decision-1-text", null),
@@ -49,15 +58,18 @@ public class ArchitectureUpdateValidatorTest {
                 )
                 .TDDs(
                         Map.of(
-                                new Tdd.ComponentReference("Component-0"), List.of(
+                                new Tdd.ComponentReference("Component-38"), List.of(
                                         new Tdd(new Tdd.Id("Valid-TDD-with-requirement-and-story"), "text")
                                 ),
-                                new Tdd.ComponentReference("Component-1"), List.of(
+                                new Tdd.ComponentReference("Component-14"), List.of(
                                         new Tdd(new Tdd.Id("TDD-unused-and-without-story"), "text")
                                 ),
-                                new Tdd.ComponentReference("Component-2"), List.of(
+                                new Tdd.ComponentReference("Component-15"), List.of(
                                         new Tdd(new Tdd.Id("Valid-TDD-with-decision-and-story"), "text"),
                                         new Tdd(new Tdd.Id("TDD-unused-with-story"), "text")
+                                ),
+                                new Tdd.ComponentReference("Component-Invalid-Component-Id"), List.of(
+                                        new Tdd(new Tdd.Id("Tdd-with-invalid-component"), "text")
                                 )
                         )
                 )
@@ -80,9 +92,7 @@ public class ArchitectureUpdateValidatorTest {
 
     @Test
     public void blankAuShouldBeValid() {
-        var au = ArchitectureUpdate.blank();
-
-        var result = ArchitectureUpdateValidator.validate(au);
+        var result = ArchitectureUpdateValidator.validate(ArchitectureUpdate.blank());
 
         collector.checkThat(result.isValid(), is(true));
         collector.checkThat(result.isValid(ValidationStage.CAPABILITY), is(true));
@@ -91,9 +101,9 @@ public class ArchitectureUpdateValidatorTest {
 
     @Test
     public void shouldFindAllErrors() {
-        ArchitectureUpdate au = invalidAu;
+        ArchitectureDataStructure data = validDataStructure;
 
-        var actualErrors = ArchitectureUpdateValidator.validate(au).getErrors();
+        var actualErrors = ArchitectureUpdateValidator.validate(invalidAu).getErrors();
         var expectedErrors = List.of(
                 ValidationError.forInvalidTddReference(new Decision.Id("Bad-TDD-Decision"), new Tdd.Id("BAD-TDD-ID")),
                 ValidationError.forInvalidTddReference(new FunctionalRequirement.Id("Bad-TDD-Functional-Requirement"), new Tdd.Id("BAD-TDD-ID")),
@@ -104,7 +114,9 @@ public class ArchitectureUpdateValidatorTest {
                 ValidationError.forTddWithoutStory(new Tdd.Id("TDD-unused-and-without-story")),
 
                 ValidationError.forTddWithoutCause(new Tdd.Id("TDD-unused-and-without-story")),
-                ValidationError.forTddWithoutCause(new Tdd.Id("TDD-unused-with-story"))
+                ValidationError.forTddWithoutCause(new Tdd.Id("TDD-unused-with-story")),
+
+                ValidationError.forInvalidComponentReference(new Tdd.Id("Tdd-with-invalid-component"), new Tdd.ComponentReference("Component-Invalid-Component-Id"))
         );
 
         collector.checkThat(actualErrors.size(), equalTo(expectedErrors.size()));

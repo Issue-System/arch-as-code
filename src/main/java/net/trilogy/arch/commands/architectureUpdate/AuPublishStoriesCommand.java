19b615947ca9c02221b3ce4bf2ee85c8cc460ad0
nikolasavic-tw <nsavic@thoughtworks.com>
2020-05-01 09:56:03 -0400

    Add args to AU Publish command

--- src/main/java/net/trilogy/arch/commands/architectureUpdate/AuPublishStoriesCommand.java
+++ src/main/java/net/trilogy/arch/commands/architectureUpdate/AuPublishStoriesCommand.java
@@ -1,27 +1,42 @@
 package net.trilogy.arch.commands.architectureUpdate;
 
+import net.trilogy.arch.adapter.ArchitectureUpdateObjectMapper;
 import net.trilogy.arch.adapter.FilesFacade;
 import net.trilogy.arch.adapter.Jira.JiraApi;
 import net.trilogy.arch.adapter.Jira.JiraApiFactory;
+import net.trilogy.arch.adapter.in.ArchitectureDataStructureReader;
+import net.trilogy.arch.domain.ArchitectureDataStructure;
+import net.trilogy.arch.domain.architectureUpdate.ArchitectureUpdate;
+import net.trilogy.arch.validation.architectureUpdate.ArchitectureUpdateValidator;
 import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "publish", description = "Publish stories.")
 public class AuPublishStoriesCommand implements Callable<Integer> {
 
     private final JiraApiFactory jiraApiFactory;
-    private FilesFacade filesFacade;
+    private final FilesFacade filesFacade;
+
+    @CommandLine.Parameters(index = "0", description = "File name of architecture update to validate")
+    private String architectureUpdateFileName;
+
+    @CommandLine.Parameters(index = "1", description = "Product documentation root directory")
+    private File productDocumentationRoot;
+
     @CommandLine.Option(names = {"-u", "--username"}, description = "Username")
     private File username;
 
     @CommandLine.Option(names = {"-p", "--password"}, arity = "0..1", interactive = true)
     private char[] password;
 
-    @CommandLine.Parameters(index = "0", description = "Product documentation root directory")
-    private File productDocumentationRoot;
+    @CommandLine.Spec
+    private CommandLine.Model.CommandSpec spec;
+
 
     public AuPublishStoriesCommand(JiraApiFactory jiraApiFactory, FilesFacade filesFacade) {
         this.jiraApiFactory = jiraApiFactory;
@@ -29,6 +44,20 @@ public class AuPublishStoriesCommand implements Callable<Integer> {
     }
 
     public Integer call() throws IOException, InterruptedException {
+
+        Path auPath = productDocumentationRoot.toPath()
+                .resolve(architectureUpdateFileName)
+                .toAbsolutePath();
+
+        try {
+            ArchitectureDataStructure architecture = new ArchitectureDataStructureReader()
+                    .load(productDocumentationRoot.toPath().resolve("data-structure.yml").toFile());
+            ArchitectureUpdate au = new ArchitectureUpdateObjectMapper().readValue(Files.readString(auPath));
+        } catch (IOException | RuntimeException e) {
+            spec.commandLine().getErr().println("Invalid structure. Error thrown: \n" + e.getMessage() + "\nCause: " + e.getCause());
+            return 1;
+        }
+
 
         final JiraApi jiraApi = jiraApiFactory.create(filesFacade);
         jiraApi.createStory();
--- src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuPublishStoriesCommandTest.java
+++ src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuPublishStoriesCommandTest.java
@@ -1,32 +1,40 @@
 package net.trilogy.arch.e2e.architectureUpdate;
 
 import net.trilogy.arch.Application;
+import net.trilogy.arch.TestHelper;
 import net.trilogy.arch.adapter.FilesFacade;
 import net.trilogy.arch.adapter.Jira.JiraApi;
 import net.trilogy.arch.adapter.Jira.JiraApiFactory;
 import net.trilogy.arch.adapter.in.google.GoogleDocsAuthorizedApiFactory;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static net.trilogy.arch.TestHelper.execute;
 import static org.mockito.Mockito.*;
 
 public class AuPublishStoriesCommandTest {
 
+    private File rootDir;
+    private JiraApi mockedJiraApi;
+    private Application app;
+
+    @Before
+    public void setUp() throws Exception {
+        rootDir = new File(getClass().getResource(TestHelper.ROOT_PATH_TO_TEST_AU_PUBLISH).getPath());
+        FilesFacade filesFacade = new FilesFacade();
+        GoogleDocsAuthorizedApiFactory mockedGoogleApiFactory = mock(GoogleDocsAuthorizedApiFactory.class);
+        final JiraApiFactory mockedJiraApiFactory = mock(JiraApiFactory.class);
+        mockedJiraApi = mock(JiraApi.class);
+        when(mockedJiraApiFactory.create(filesFacade)).thenReturn(mockedJiraApi);
+        app = new Application(mockedGoogleApiFactory, mockedJiraApiFactory, filesFacade);
+    }
+
     @Test
     public void shouldCreateJiraStories() throws IOException, InterruptedException {
-        final Path rootDir = Files.createTempDirectory("arch-as-code_architecture-update_command_tests");
-        final FilesFacade filesFacade = new FilesFacade();
-        final GoogleDocsAuthorizedApiFactory mockedGoogleApiFactory = mock(GoogleDocsAuthorizedApiFactory.class);
-        final JiraApiFactory mockedJiraApiFactory = mock(JiraApiFactory.class);
-        final JiraApi mockedJiraApi = mock(JiraApi.class);
-        when(mockedJiraApiFactory.create(filesFacade)).thenReturn(mockedJiraApi);
-
-        final Application app = new Application(mockedGoogleApiFactory, mockedJiraApiFactory, filesFacade);
-        execute(app, "au publish -u user -p password " + rootDir.toAbsolutePath().toString());
+        execute(app, "au publish -u user -p password architecture-updates/test.yml " + rootDir.getAbsolutePath());
 
         verify(mockedJiraApi).createStory();
     }

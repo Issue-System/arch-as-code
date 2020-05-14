061d83f4029ef0608d681493c278851e79db23d9
nikolasavic-tw <nsavic@thoughtworks.com>
2020-05-14 10:46:07 -0400

    Add error messages for au annotate

--- src/main/java/net/trilogy/arch/commands/architectureUpdate/AuAnnotateCommand.java
+++ src/main/java/net/trilogy/arch/commands/architectureUpdate/AuAnnotateCommand.java
@@ -1,12 +1,5 @@
 package net.trilogy.arch.commands.architectureUpdate;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.Callable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import lombok.SneakyThrows;
 import net.trilogy.arch.adapter.FilesFacade;
 import net.trilogy.arch.adapter.in.ArchitectureDataStructureReader;
 import net.trilogy.arch.domain.ArchitectureDataStructure;
@@ -14,6 +7,11 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Spec;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Command(name = "annotate", description = "Annotates the architecture update with comments detailing the full paths of all components referenced by ID. Makes the AU easier to read.", mixinStandardHelpOptions = true)
 public class AuAnnotateCommand implements Callable<Integer> {
@@ -33,23 +31,42 @@ public class AuAnnotateCommand implements Callable<Integer> {
         this.filesFacade = filesFacade;
     }
 
-    @SneakyThrows
     @Override
     public Integer call() {
         var regexToGetComponentReferences = Pattern.compile("(\\n\\s+['\"]?Component-)(\\d+)(['\"]?:)(\\n)");
 
-        var au = filesFacade.readString(architectureUpdateFilePath.toPath());
+        String au = null;
+        try {
+            au = filesFacade.readString(architectureUpdateFilePath.toPath());
+        } catch (Exception e) {
+            spec.commandLine().getErr().println("Unable to load Architecture Update." +
+                    "\nError: " + e + "\nCause: " + e.getCause());
+            return 2;
+        }
 
         final Matcher matcher = regexToGetComponentReferences.matcher(au);
 
-        var architecture = new ArchitectureDataStructureReader()
-                .load(productDocumentationRoot.toPath().resolve("data-structure.yml").toFile());
+        final ArchitectureDataStructure architecture;
+        try {
+            architecture = new ArchitectureDataStructureReader()
+                    .load(productDocumentationRoot.toPath().resolve("data-structure.yml").toFile());
+        } catch (Exception e) {
+            spec.commandLine().getErr().println("Unable to load Architecture data-structure.yml." +
+                    "\nError: " + e + "\nCause: " + e.getCause());
+            return 2;
+        }
 
         while (matcher.find()) {
             au = matcher.replaceAll((res) -> res.group(1) + res.group(2) + res.group(3) + getComponentPathComment(res.group(2), architecture) + res.group(4));
         }
 
-        filesFacade.writeString(architectureUpdateFilePath.toPath(), au); 
+        try {
+            filesFacade.writeString(architectureUpdateFilePath.toPath(), au);
+        } catch (Exception e) {
+            spec.commandLine().getErr().println("Unable to write annotations to Architecture Update." +
+                    "\nError: " + e + "\nCause: " + e.getCause());
+            return 2;
+        }
 
         return 0;
     }
--- src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuAnnotateCommandTest.java
+++ src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuAnnotateCommandTest.java
@@ -1,22 +1,24 @@
 package net.trilogy.arch.e2e.architectureUpdate;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
+import net.trilogy.arch.Application;
+import net.trilogy.arch.TestHelper;
+import net.trilogy.arch.adapter.FilesFacade;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
-import net.trilogy.arch.Application;
-import net.trilogy.arch.TestHelper;
-import net.trilogy.arch.adapter.FilesFacade;
-import net.trilogy.arch.adapter.in.ArchitectureDataStructureReader;
-import net.trilogy.arch.domain.architectureUpdate.ArchitectureUpdate;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class AuAnnotateCommandTest {
     @Rule
@@ -26,19 +28,31 @@ public class AuAnnotateCommandTest {
     Path changedAuPath;
     Path originalAuPath;
 
+    final PrintStream originalOut = System.out;
+    final PrintStream originalErr = System.err;
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    final ByteArrayOutputStream err = new ByteArrayOutputStream();
+
     @Before
     public void setUp() throws Exception {
         rootPath = TestHelper.getPath(getClass(), TestHelper.ROOT_PATH_TO_TEST_AU_ANNOTATE);
         originalAuPath = rootPath.resolve("architecture-updates/valid-with-components.yml");
         changedAuPath = rootPath.resolve("architecture-updates/valid-with-components-clone.yml");
 
-        Files.copy(rootPath.resolve("architecture-updates/valid-with-components.yml"),
-                changedAuPath);
+        Files.copy(rootPath.resolve("architecture-updates/valid-with-components.yml"), changedAuPath);
+
+        out.reset();
+        err.reset();
+        System.setOut(new PrintStream(out));
+        System.setErr(new PrintStream(err));
     }
 
     @After
     public void tearDown() throws Exception {
         Files.deleteIfExists(changedAuPath);
+
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 
     @Test
@@ -61,11 +75,55 @@ public class AuAnnotateCommandTest {
         collector.checkThat(actual, equalTo(expected));
     }
 
-    // TODO [TESTING] [ENHANCEMENT] : What happens if unable to load architecture?
-    // TODO [TESTING] [ENHANCEMENT] : What happens if unable to load au?
+    @Test
+    public void shouldNotifyUserWhenAUFailsToLoad() throws Exception {
+        // GIVEN
+        final FilesFacade mockedFilesFacade = spy(FilesFacade.class);
+        when(mockedFilesFacade.readString(changedAuPath)).thenThrow(new IOException("error-message", new RuntimeException("Boom!")));
+
+        // WHEN
+        int status = TestHelper.execute(new Application(null, null, mockedFilesFacade),
+                "au annotate " + str(changedAuPath) + " " + str(rootPath));
+
+        // THEN
+        collector.checkThat(out.toString(), equalTo(""));
+        collector.checkThat(err.toString(), equalTo("Unable to load Architecture Update.\nError: java.io.IOException: error-message\nCause: java.lang.RuntimeException: Boom!\n"));
+        collector.checkThat(status, equalTo(2));
+    }
+
+    @Test
+    public void shouldNotifyUserWhenArchitectureDatastructureFailsToLoad() {
+        // TODO: Replace use files facade in architecture datastructure .load()
+        // WHEN
+        int status = TestHelper.execute(new Application(null, null, new FilesFacade()),
+                "au annotate " + str(changedAuPath) + " " + "missing-file-path");
+
+        // THEN
+        collector.checkThat(out.toString(), equalTo(""));
+        collector.checkThat(err.toString(), containsString("Unable to load Architecture data-structure.yml."));
+        collector.checkThat(status, equalTo(2));
+    }
+
+    @Test
+    public void shouldNotifuUserWhenAnnotationFailsToWrite() throws Exception {
+        // GIVEN
+        final FilesFacade mockedFilesFacade = mock(FilesFacade.class);
+        when(mockedFilesFacade.readString(any())).thenCallRealMethod();
+        when(mockedFilesFacade.writeString(any(), any())).thenThrow(new IOException("Ran out of bytes!"));
+
+        // WHEN
+        int status = TestHelper.execute(new Application(null, null, mockedFilesFacade),
+                "au annotate " + str(changedAuPath) + " " + str(rootPath));
+
+        // THEN
+        collector.checkThat(out.toString(), equalTo(""));
+        collector.checkThat(err.toString(), equalTo("Unable to write annotations to Architecture Update.\nError: java.io.IOException: Ran out of bytes!\nCause: null\n"));
+        collector.checkThat(status, equalTo(2));
+
+    }
+
     // TODO [TESTING] [ENHANCEMENT] : What happens if component reference is invalid?
-    // TODO [TESTING] [ENHANCEMENT] : What happens if writing IO fails?
-    // TODO [TESTING] [ENHANCEMENT] : What happens if there are no components? 
+    // TODO [TESTING] [ENHANCEMENT] : What happens if there are no components?
 
     private String str(Path path) {
         return path.toAbsolutePath().toString();

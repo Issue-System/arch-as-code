0140e9b9fabae48c930ee3cfa601eee0040bd39e
Yogesh Dhamija <yogesh.dhamija@thoughtworks.com>
2020-05-14 13:31:00 -0400

    au annotate command handles sad paths

--- src/main/java/net/trilogy/arch/commands/architectureUpdate/AuAnnotateCommand.java
+++ src/main/java/net/trilogy/arch/commands/architectureUpdate/AuAnnotateCommand.java
@@ -39,8 +39,7 @@ public class AuAnnotateCommand implements Callable<Integer> {
         try {
             au = filesFacade.readString(architectureUpdateFilePath.toPath());
         } catch (Exception e) {
-            spec.commandLine().getErr().println("Unable to load Architecture Update." +
-                    "\nError: " + e + "\nCause: " + e.getCause());
+            printError(e, "Unable to load Architecture Update.");
             return 2;
         }
 
@@ -48,30 +47,40 @@ public class AuAnnotateCommand implements Callable<Integer> {
 
         final ArchitectureDataStructure architecture;
         try {
-            architecture = new ArchitectureDataStructureReader(filesFacade)
-                    .load(productDocumentationRoot.toPath().resolve("data-structure.yml").toFile());
+            architecture = new ArchitectureDataStructureReader(filesFacade).load(
+                    productDocumentationRoot.toPath().resolve("data-structure.yml").toFile()
+            );
         } catch (Exception e) {
-            spec.commandLine().getErr().println("Unable to load Architecture data-structure.yml." +
-                    "\nError: " + e + "\nCause: " + e.getCause());
+            printError(e, "Unable to load Architecture data-structure.yml.");
             return 2;
         }
 
         while (matcher.find()) {
-            au = matcher.replaceAll((res) -> res.group(1) + res.group(2) + res.group(3) + getComponentPathComment(res.group(2), architecture) + res.group(4));
+            au = matcher.replaceAll((res) -> {
+                return res.group(1) + res.group(2) + res.group(3) + getComponentPathComment(res.group(2), architecture) + res.group(4);
+            });
         }
 
         try {
             filesFacade.writeString(architectureUpdateFilePath.toPath(), au);
         } catch (Exception e) {
-            spec.commandLine().getErr().println("Unable to write annotations to Architecture Update." +
-                    "\nError: " + e + "\nCause: " + e.getCause());
+            printError(e, "Unable to write annotations to Architecture Update.");
             return 2;
         }
 
         return 0;
     }
 
+    private void printError(Exception e, String s) {
+        spec.commandLine().getErr().println(s +
+                "\nError: " + e + "\nCause: " + e.getCause());
+    }
+
     private String getComponentPathComment(String id, ArchitectureDataStructure architecture) {
-        return "  # " + architecture.getModel().findEntityById(id).getPath().getPath();
+        try {
+            return "  # " + architecture.getModel().findEntityById(id).getPath().getPath();
+        } catch (Exception ignored) {
+            return "";
+        }
     }
 }
--- src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuAnnotateCommandTest.java
+++ src/test/java/net/trilogy/arch/e2e/architectureUpdate/AuAnnotateCommandTest.java
@@ -27,8 +27,10 @@ public class AuAnnotateCommandTest {
     public final ErrorCollector collector = new ErrorCollector();
 
     Path rootPath;
-    Path changedAuPath;
-    Path originalAuPath;
+    Path originalAuWithComponentsPath;
+    Path changedAuWithComponentsPath;
+    Path originalAuWithoutComponentsPath;
+    Path changedAuWithoutComponentsPath;
 
     final PrintStream originalOut = System.out;
     final PrintStream originalErr = System.err;
@@ -38,10 +40,14 @@ public class AuAnnotateCommandTest {
     @Before
     public void setUp() throws Exception {
         rootPath = TestHelper.getPath(getClass(), TestHelper.ROOT_PATH_TO_TEST_AU_ANNOTATE);
-        originalAuPath = rootPath.resolve("architecture-updates/valid-with-components.yml");
-        changedAuPath = rootPath.resolve("architecture-updates/valid-with-components-clone.yml");
+        originalAuWithComponentsPath = rootPath.resolve("architecture-updates/valid-with-components.yml");
+        originalAuWithoutComponentsPath = rootPath.resolve("architecture-updates/valid-without-components.yml");
 
-        Files.copy(rootPath.resolve("architecture-updates/valid-with-components.yml"), changedAuPath);
+        changedAuWithComponentsPath = rootPath.resolve("architecture-updates/valid-with-components-clone.yml");
+        changedAuWithoutComponentsPath = rootPath.resolve("architecture-updates/valid-without-components-clone.yml");
+
+        Files.copy(originalAuWithComponentsPath, changedAuWithComponentsPath);
+        Files.copy(originalAuWithoutComponentsPath, changedAuWithoutComponentsPath);
 
         out.reset();
         err.reset();
@@ -51,7 +57,8 @@ public class AuAnnotateCommandTest {
 
     @After
     public void tearDown() throws Exception {
-        Files.deleteIfExists(changedAuPath);
+        Files.deleteIfExists(changedAuWithComponentsPath);
+        Files.deleteIfExists(changedAuWithoutComponentsPath);
 
         System.setOut(originalOut);
         System.setErr(originalErr);
@@ -61,13 +68,14 @@ public class AuAnnotateCommandTest {
     public void shouldAnnotate() throws Exception {
         // WHEN
         int status = TestHelper.execute(new Application(null, null, new FilesFacade()),
-                "au annotate " + str(changedAuPath) + " " + str(rootPath));
+                "au annotate " + str(changedAuWithComponentsPath) + " " + str(rootPath));
 
-        var actual = Files.readString(changedAuPath);
+        var actual = Files.readString(changedAuWithComponentsPath);
 
         // THEN
-        var expected = Files.readString(originalAuPath).replaceFirst("'Component-31':",
-                "'Component-31':  # c4://Internet Banking System/API Application/Reset Password Controller")
+        var expected = Files.readString(originalAuWithComponentsPath)
+                .replaceFirst("'Component-31':",
+                        "'Component-31':  # c4://Internet Banking System/API Application/Reset Password Controller")
                 .replaceFirst("Component-30:",
                         "Component-30:  # c4://Internet Banking System/API Application/Accounts Summary Controller")
                 .replaceFirst("\"Component-34\":",
@@ -78,14 +86,52 @@ public class AuAnnotateCommandTest {
     }
 
     @Test
+    public void shouldHandleNoComponents() throws Exception {
+        // WHEN
+        int status = TestHelper.execute(new Application(null, null, new FilesFacade()),
+                "au annotate " + str(changedAuWithoutComponentsPath) + " " + str(rootPath));
+
+        var actual = Files.readString(changedAuWithoutComponentsPath);
+
+        // THEN
+        var expected = Files.readString(originalAuWithoutComponentsPath);
+
+        collector.checkThat(actual, equalTo(expected));
+        collector.checkThat(status, equalTo(0));
+    }
+
+    @Test
+    public void shouldIgnoreInvalidComponents() throws Exception {
+        // GIVEN
+        Files.writeString(changedAuWithComponentsPath, Files.readString(changedAuWithComponentsPath).replace("Component-31", "Component-404"));
+
+        // WHEN
+        int status = TestHelper.execute(new Application(null, null, new FilesFacade()),
+                "au annotate " + str(changedAuWithComponentsPath) + " " + str(rootPath));
+
+        var actual = Files.readString(changedAuWithComponentsPath);
+
+        // THEN
+        var expected = Files.readString(originalAuWithComponentsPath)
+                .replaceFirst("'Component-31':", "'Component-404':")
+                .replaceFirst("Component-30:",
+                        "Component-30:  # c4://Internet Banking System/API Application/Accounts Summary Controller")
+                .replaceFirst("\"Component-34\":",
+                        "\"Component-34\":  # c4://Internet Banking System/API Application/E-mail Component");
+
+        collector.checkThat(actual, equalTo(expected));
+        collector.checkThat(status, equalTo(0));
+    }
+
+    @Test
     public void shouldNotifyUserWhenAUFailsToLoad() throws Exception {
         // GIVEN
         final FilesFacade mockedFilesFacade = spy(FilesFacade.class);
-        when(mockedFilesFacade.readString(changedAuPath)).thenThrow(new IOException("error-message", new RuntimeException("Boom!")));
+        when(mockedFilesFacade.readString(changedAuWithComponentsPath)).thenThrow(new IOException("error-message", new RuntimeException("Boom!")));
 
         // WHEN
         int status = TestHelper.execute(new Application(null, null, mockedFilesFacade),
-                "au annotate " + str(changedAuPath) + " " + str(rootPath));
+                "au annotate " + str(changedAuWithComponentsPath) + " " + str(rootPath));
 
         // THEN
         collector.checkThat(out.toString(), equalTo(""));
@@ -104,7 +150,7 @@ public class AuAnnotateCommandTest {
 
         // WHEN
         int status = TestHelper.execute(new Application(null, null, spyedFilesFacade),
-                "au annotate " + str(changedAuPath) + " " + str(rootPath));
+                "au annotate " + str(changedAuWithComponentsPath) + " " + str(rootPath));
 
         // THEN
         collector.checkThat(out.toString(), equalTo(""));
@@ -123,7 +169,7 @@ public class AuAnnotateCommandTest {
 
         // WHEN
         int status = TestHelper.execute(new Application(null, null, mockedFilesFacade),
-                "au annotate " + str(changedAuPath) + " " + str(rootPath));
+                "au annotate " + str(changedAuWithComponentsPath) + " " + str(rootPath));
 
         // THEN
         collector.checkThat(out.toString(), equalTo(""));
@@ -133,9 +179,6 @@ public class AuAnnotateCommandTest {
         collector.checkThat(status, equalTo(2));
 
     }
-
-    // TODO [TESTING] [ENHANCEMENT] : What happens if component reference is invalid?
-    // TODO [TESTING] [ENHANCEMENT] : What happens if there are no components?
 
     private String str(Path path) {
         return path.toAbsolutePath().toString();

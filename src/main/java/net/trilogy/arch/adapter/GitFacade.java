884f5405fc0360ebea73413fbf6ecbb6ec208e39
Yogesh Dhamija <yogesh.dhamija@thoughtworks.com>
2020-05-28 12:00:18 -0400

    fix au new command not detecting git branch

--- src/main/java/net/trilogy/arch/adapter/GitFacade.java
+++ src/main/java/net/trilogy/arch/adapter/GitFacade.java
@@ -1,7 +1,6 @@
 package net.trilogy.arch.adapter;
 
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.File;
@@ -9,15 +8,14 @@ import java.io.IOException;
 
 public class GitFacade {
     public Git openParentRepo(File dir) throws IOException {
-        return Git.wrap(getRepository(dir));
+        return Git.wrap(
+            new FileRepositoryBuilder()
+                .findGitDir(toAbsolute(dir))
+                .build()
+        );
     }
 
-    private static Repository getRepository(File rootDir) throws IOException {
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        return builder
-                .readEnvironment()
-                .findGitDir(rootDir)
-                .build();
+    private static File toAbsolute(File dir){
+        return dir.toPath().toAbsolutePath().toFile();
     }
-
 }

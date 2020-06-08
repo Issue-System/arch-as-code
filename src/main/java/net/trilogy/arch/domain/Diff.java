85d48f09a630bfd87e3990ba445a38234319fd89
Yogesh Dhamija <yogesh.dhamija@thoughtworks.com>
2020-06-08 12:56:27 -0500

    diff object handles descendants

--- src/main/java/net/trilogy/arch/domain/Diff.java
+++ src/main/java/net/trilogy/arch/domain/Diff.java
@@ -1,5 +1,7 @@
 package net.trilogy.arch.domain;
 
+import java.util.Set;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,13 +10,26 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 public class Diff{
-    final private Diffable before;
-    final private Diffable after;
+    final private Diffable<?> before;
+    final private Diffable<?> after;
+    final private Set<? extends Diffable<?>> descendantsBefore;
+    final private Set<? extends Diffable<?>> descendantsAfter;
     private Status status;
 
-    public Diff(Diffable before, Diffable after) {
+    @Deprecated
+    public Diff(Diffable<?> before, Diffable<?> after) {
         this.before = before;
         this.after = after;
+        this.descendantsAfter = Set.of();
+        this.descendantsBefore = Set.of();
+        this.status = calculateStatus();
+    }
+
+    public Diff(Diffable<?> before, Set<? extends Diffable<?>> descendantsBefore, Diffable<?> after, Set<? extends Diffable<?>> descendantsAfter) {
+        this.before = before;
+        this.after = after;
+        this.descendantsAfter = descendantsBefore;
+        this.descendantsBefore = descendantsAfter;
         this.status = calculateStatus();
     }
 
@@ -22,15 +37,9 @@ public class Diff{
         if (before == null && after == null) throw new UnsupportedOperationException();
         if (before == null) return Status.CREATED;
         if (after == null) return Status.DELETED;
-        if (before.equals(after)) return Status.NO_UPDATE;
-
-        return Status.UPDATED;
-    }
-
-    public void markChildrenUpdated() {
-        if (status.equals(Status.NO_UPDATE)) {
-            this.status = Status.NO_UPDATE_BUT_CHILDREN_UPDATED;
-        }
+        if (!before.equals(after)) return Status.UPDATED;
+        if (!descendantsBefore.equals(descendantsAfter)) return Status.NO_UPDATE_BUT_CHILDREN_UPDATED;
+        return Status.NO_UPDATE;
     }
 
     public enum Status {
--- src/test/java/net/trilogy/arch/domain/DiffTest.java
+++ src/test/java/net/trilogy/arch/domain/DiffTest.java
@@ -8,48 +8,69 @@ import lombok.Getter;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Set;
 
 public class DiffTest {
 
     @Test
     public void shouldCalculateCreatedStatus() {
-        final Diff diff = new Diff(null, new Thing("toBeCreated"));
+        final Diff diff = new Diff(
+                null, Set.of(),
+                new Thing("toBeCreated"), Set.of()
+        );
         assertThat(diff.getStatus(), equalTo(Diff.Status.CREATED));
     }
 
     @Test
     public void shouldCalculateDeletedStatus() {
-        final Diff diff = new Diff(new Thing("toBeDeleted"), null);
+        final Diff diff = new Diff(
+                new Thing("toBeDeleted"), Set.of(),
+                null, Set.of()
+        );
         assertThat(diff.getStatus(), equalTo(Diff.Status.DELETED));
     }
 
     @Test
     public void shouldCalculateUpdatedStatus() {
-        final Diff diff = new Diff(new Thing("toBeUpdated"), new Thing("updated"));
+        final Diff diff = new Diff(
+                new Thing("toBeUpdated"), Set.of(),
+                new Thing("updated"), Set.of()
+        );
         assertThat(diff.getStatus(), equalTo(Diff.Status.UPDATED));
     }
 
     @Test
     public void shouldCalculateNoUpdateStatus() {
-        final Diff diff = new Diff(new Thing("noChange"), new Thing("noChange"));
+        final Diff diff = new Diff(
+                new Thing("noChange"), Set.of(),
+                new Thing("noChange"), Set.of()
+        );
         assertThat(diff.getStatus(), equalTo(Diff.Status.NO_UPDATE));
     }
 
     @Test
     public void shouldCalculatedChildrenUpdated() {
-        final Diff diff = new Diff(new Thing("noUpdate"), new Thing("noUpdate"));
-        diff.markChildrenUpdated();
+        final Diff diff = new Diff(
+                new Thing("noUpdate"), Set.of(new Thing("a")),
+                new Thing("noUpdate"), Set.of(new Thing("b"))
+        );
         assertThat(diff.getStatus(), equalTo(Diff.Status.NO_UPDATE_BUT_CHILDREN_UPDATED));
     }
 
     @Test
     public void shouldNotCalculateChildrenIfChangedStatus() {
-        final Diff created = new Diff(null, new Thing("toBeCreated"));
-        final Diff deleted = new Diff(new Thing("toBeDeleted"), null);
-        final Diff updated = new Diff(new Thing("toBeUpdated"), new Thing("updated"));
-        created.markChildrenUpdated();
-        deleted.markChildrenUpdated();
-        updated.markChildrenUpdated();
+        final Diff created = new Diff(
+                null, Set.of(new Thing("a")),
+                new Thing("toBeCreated"), Set.of(new Thing("b"))
+        );
+        final Diff deleted = new Diff(
+                new Thing("toBeDeleted"), Set.of(new Thing("a")),
+                null, Set.of(new Thing("b"))
+        );
+        final Diff updated = new Diff(
+                new Thing("toBeUpdated"), Set.of(new Thing("a")),
+                new Thing("updated"), Set.of(new Thing("b"))
+        );
 
         assertThat(created.getStatus(), equalTo(Diff.Status.CREATED));
         assertThat(deleted.getStatus(), equalTo(Diff.Status.DELETED));
@@ -58,9 +79,9 @@ public class DiffTest {
 
     @EqualsAndHashCode
     @Getter
-    private static class Thing implements Diffable {
+    private static class Thing implements Diffable<Thing> {
         private final String id;
         public Thing(String id) { this.id = id; }
-        public Diffable shallowCopy() { return this; }
+        public Thing shallowCopy() { return this; }
     }
 }

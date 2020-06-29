90148dcf3d14bf6674fd583c0659987a18fed4d5
Nikola Savic <nsavic@thoughtworks.com>
2020-06-29 10:58:17 -0400

    Update Big Bank workspace to most recent version on Structurizr.com

--- src/test/java/net/trilogy/arch/TestHelper.java
+++ src/test/java/net/trilogy/arch/TestHelper.java
@@ -24,7 +24,7 @@ public abstract class TestHelper {
     public static final String MANIFEST_PATH_TO_TEST_MODEL_DEPLOYMENT_NODES = "/view/bigBank/product-architecture.yml";
     public static final String MANIFEST_PATH_TO_TEST_VIEWS = "/view/bigBank/product-architecture.yml";
 
-    public static final String JSON_STRUCTURIZR_BIG_BANK = "/structurizr/BigBank.json";
+    public static final String JSON_STRUCTURIZR_BIG_BANK = "/structurizr/big-bank.json";
     public static final String JSON_STRUCTURIZR_THINK3_SOCOCO = "/structurizr/Think3-Sococo.c4model.json";
     public static final String JSON_STRUCTURIZR_EMPTY = "/structurizr/empty-workspace.json";
     public static final String JSON_STRUCTURIZR_NO_SYSTEM = "/structurizr/no-system-deployment-view-workspace.json";

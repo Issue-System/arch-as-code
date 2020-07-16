16d641ec5206a77e99cec2ad08dc1618eb2aaf7b
Nikola Savic <nsavic@thoughtworks.com>
2020-07-16 17:05:05 -0400

    Remove unnecessary path object nesting during serialization

--- src/main/java/net/trilogy/arch/domain/c4/C4Tag.java
+++ src/main/java/net/trilogy/arch/domain/c4/C4Tag.java
@@ -1,15 +1,12 @@
 package net.trilogy.arch.domain.c4;
 
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class C4Tag {
     @NonNull
     String tag;
--- src/main/java/net/trilogy/arch/domain/c4/Entity.java
+++ src/main/java/net/trilogy/arch/domain/c4/Entity.java
@@ -1,6 +1,8 @@
 package net.trilogy.arch.domain.c4;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
@@ -11,10 +13,12 @@ import static java.util.Optional.ofNullable;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode
 public abstract class Entity implements HasRelation, HasTag, HasIdentity, Comparable<Entity> {
     @NonNull
     protected String id;
     protected String alias;
+    @JsonUnwrapped
     protected C4Path path;
     @NonNull
     protected String name;
@@ -39,5 +43,23 @@ public abstract class Entity implements HasRelation, HasTag, HasIdentity, Compar
         this.tags = ofNullable(tags).orElse(emptySet());
         this.relationships = ofNullable(relationships).orElse(emptySet());
         this.name = name;
+    }
+
+    public void setPath(String path) {
+        if (path == null) {
+            this.path = null;
+            return;
+        }
+
+        this.path = new C4Path(path);
+    }
+
+    public void setPath(C4Path path) {
+        if (path == null) {
+            this.path = null;
+            return;
+        }
+
+        this.path = path;
     }
 }
